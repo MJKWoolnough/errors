8895af9956a58dd81ffbc57c35507b8059db0871
Michael Woolnough <michael.woolnough@gmail.com>
2016-08-05 17:28:11 +0100

    added error tracing

--- errors.go
+++ errors.go
@@ -1,7 +1,88 @@
 package errors
 
+import "runtime"
+
 type Error string
 
 func (e Error) Error() string {
 	return string(e)
+}
+
+type Call struct {
+	File, Function string
+	LineNum        int
+}
+
+func (c Call) String() string {
+	return c.File + ": (" + string(itobs(c.LineNum)) + ")" + c.Function
+}
+
+type Trace struct {
+	error
+	Traces []Call
+}
+
+func AddTrace(e error) error {
+	var trace [100]uintptr
+	num := runtime.Callers(2, trace[:])
+	traces := make([]Call, num)
+	for i := 0; i < num; i++ {
+		f := runtime.FuncForPC(trace[i])
+		file, ln := f.FileLine(trace[i])
+		traces[i] = Call{
+			File:     file,
+			Function: f.Name(),
+			LineNum:  ln,
+		}
+	}
+	return &Trace{
+		error:  e,
+		Traces: traces,
+	}
+}
+
+func (t Trace) Trace() []byte {
+	var buf []byte
+	for _, c := range t.Traces {
+		buf = append(buf, c.File...)
+		buf = append(buf, ':', ' ', '(')
+		buf = append(buf, itobs(c.LineNum)...)
+		buf = append(buf, ')', ' ')
+		buf = append(buf, c.Function...)
+		buf = append(buf, '\n')
+	}
+	return buf
+}
+
+type Underlyer interface {
+	Underlying() error
+}
+
+func (t Trace) Underlying() error {
+	if u, ok := t.error.(Underlyer); ok {
+		return u.Underlying()
+	}
+	return t.error
+}
+
+func itobs(i int) []byte {
+	if i == 0 {
+		return []byte{'0'}
+	}
+	var neg = false
+	if i < 0 {
+		neg = true
+		i = -i
+	}
+	pos := 21
+	var b [21]byte
+	for ; i > 0; i /= 10 {
+		pos--
+		b[pos] = byte(i%10) + '0'
+	}
+	if neg {
+		pos--
+		b[pos] = '-'
+	}
+	return b[pos:]
 }
--- errors_test.go
+++ errors_test.go
@@ -14,11 +14,42 @@ func TestErrors(t *testing.T) {
 	if err == B {
 		t.Errorf("not expecting err to match B")
 	}
+	if err.Error() != "A" {
+		t.Errorf("expecting error string \"A\" got %q", err.Error())
+	}
 	err = B
 	if err == A {
 		t.Errorf("not expecting err to match A")
 	}
 	if err != B {
 		t.Errorf("expecting err to match B")
+	}
+	if err.Error() != "B" {
+		t.Errorf("expecting error string \"B\" got %q", err.Error())
+	}
+}
+
+func TestItobs(t *testing.T) {
+	tests := []struct {
+		Input  int
+		Output []byte
+	}{
+		{0, []byte{'0'}},
+		{1, []byte{'1'}},
+		{2, []byte{'2'}},
+		{10, []byte{'1', '0'}},
+		{11, []byte{'1', '1'}},
+		{32, []byte{'3', '2'}},
+		{100, []byte{'1', '0', '0'}},
+		{-1, []byte{'-', '1'}},
+		{-2, []byte{'-', '2'}},
+		{-10, []byte{'-', '1', '0'}},
+		{-100, []byte{'-', '1', '0', '0'}},
+	}
+	for n, test := range tests {
+		o := itobs(test.Input)
+		if string(o) != string(test.Output) {
+			t.Errorf("test %d: expecting %s, got %s", n+1, test.Output, o)
+		}
 	}
 }
